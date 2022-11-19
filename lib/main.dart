a4cf4c9b6e18c1561723f8673961ea838e2bac23
asd4a10 <amanzholov000@gmail.com>
2022-11-19 13:46:22 +0600

    added saved menu, ability to like names and changed app bar theme

--- lib/main.dart
+++ lib/main.dart
@@ -10,19 +10,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random(); //takes random words from package
     return MaterialApp(
-      title: 'Startup Name Generator',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Startup Name Generator'),
+        title: 'Startup Name Generator',
+        theme: ThemeData(
+          appBarTheme: const AppBarTheme(
+            backgroundColor: Colors.white,
+            foregroundColor: Colors.black,
+          ),
         ),
-        body: const Center(
-          // child: Text('Hello World'),
-          child: RandomWords(),
-        ),
-      ),
-    );
+        home: const RandomWords());
   }
 }
 
@@ -35,26 +31,80 @@ class RandomWords extends StatefulWidget {
 
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[]; // NEW
+  final _saved = <WordPair>{}; // NEW
   final _biggerFont = const TextStyle(fontSize: 18); // NEW
-  @override
-  Widget build(BuildContext context) {
-    // return Text(wordPair.asPascalCase); // NEW
-    // final wordPair = WordPair.random(); // NEW
-    return ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: (context, i) {
-        if (i.isOdd) return const Divider();
-        final index = i ~/ 2;
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10));
-        }
+
+  void _pushSaved() {
+    Navigator.of(context).push(MaterialPageRoute<void>(builder: (context) {
+      final tiles = _saved.map((pair) {
         return ListTile(
           title: Text(
-            _suggestions[index].asPascalCase,
+            pair.asPascalCase,
             style: _biggerFont,
           ),
         );
-      },
-    );
+      });
+      final divided = tiles.isNotEmpty
+          ? ListTile.divideTiles(
+              context: context,
+              tiles: tiles,
+            ).toList()
+          : <Widget>[];
+
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('Saved Suggestions'),
+        ),
+        body: ListView(children: divided),
+      );
+    }));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Startup Name Generator'),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.list),
+              onPressed: _pushSaved,
+              tooltip: 'Saved Suggestions',
+            ),
+          ],
+        ),
+        body: ListView.builder(
+          padding: const EdgeInsets.all(16.0),
+          itemBuilder: (context, i) {
+            if (i.isOdd) return const Divider();
+            final index = i ~/ 2;
+            if (index >= _suggestions.length) {
+              _suggestions.addAll(generateWordPairs().take(10));
+            }
+            final alreadySaved = _saved.contains(_suggestions[index]); // NEW
+            return ListTile(
+              title: Text(
+                _suggestions[index].asPascalCase,
+                style: _biggerFont,
+              ),
+              trailing: Icon(
+                // NEW from here ...
+                alreadySaved ? Icons.favorite : Icons.favorite_border,
+                color: alreadySaved ? Colors.red : null,
+                semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
+              ),
+              onTap: () {
+                // NEW from here ...
+                setState(() {
+                  if (alreadySaved) {
+                    _saved.remove(_suggestions[index]);
+                  } else {
+                    _saved.add(_suggestions[index]);
+                  }
+                }); // to here.
+              },
+            );
+          },
+        ));
   }
 }
